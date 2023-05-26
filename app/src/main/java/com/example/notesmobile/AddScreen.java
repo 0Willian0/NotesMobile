1b136bb2b1e8dde70fe6b925ae931d350148ff1f
0WiIIian0 <84929084+0Willian0@users.noreply.github.com>
2023-05-25 21:29:03 -0300

    created main screen and add screen

---
+++ app/src/main/java/com/example/notesmobile/AddScreen.java
@@ -0,0 +1,16 @@
+package com.example.notesmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_screen);
+
+    }
+}
--- app/src/main/java/com/example/notesmobile/MainActivity.java
+++ app/src/main/java/com/example/notesmobile/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.notesmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addScreen(View v)
+    {
+        Intent add = new Intent(this, AddScreen.class);
+        startActivity(add);
     }
 }
